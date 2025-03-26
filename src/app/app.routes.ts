9855a22b1f67531bae00f12e85006294cbe80799
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-26 08:26:54 +0200

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InfoComponent } from './pages/info/info.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { FavouritesComponent } from './pages/favourites/favourites.component';
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+
+    {path: '', component: LandingComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'info/:contry_name', component: InfoComponent},
+    {path: 'info', component: InfoComponent},
+    {path: 'favourites', component: FavouritesComponent},
+    {path: 'login', component:LoginComponent},
+    {path: '**', component: ErrorComponent, redirectTo:''}
+];
