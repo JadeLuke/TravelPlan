4f8b7a1e39ea6864a7ee45ec3985fbfa29474cb8
JadeLuke <jadeluke.booysen@gmail.com>
2025-03-26 08:29:30 +0200

    added dist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { InfoComponent } from './pages/info/info.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { FavouritesComponent } from './pages/favourites/favourites.component';
-import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
 
@@ -13,6 +12,5 @@ export const routes: Routes = [
     {path: 'info/:contry_name', component: InfoComponent},
     {path: 'info', component: InfoComponent},
     {path: 'favourites', component: FavouritesComponent},
-    {path: 'login', component:LoginComponent},
     {path: '**', component: ErrorComponent, redirectTo:''}
 ];
